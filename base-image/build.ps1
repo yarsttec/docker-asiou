b7986f445e95c83e2b4efe8eefc3a666b3db0b85
Sergey Yarkin <sega.yarkin@gmail.com>
2019-03-03 22:45:52 -0500

    update base image tag

--- base-image/build.ps1
+++ base-image/build.ps1
@@ -2,7 +2,7 @@
 $BUILD_DATE = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
 
 docker build `
-    -t yarsttec/asiou:base `
+    -t yarsttec/asiou:7.6-base `
     -f Dockerfile `
     --build-arg BUILD_DATE=$BUILD_DATE `
     .
