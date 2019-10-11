93da4ed7cd7ac4a00a664119eecdbbbff9dfc16b
Sergey Yarkin <sega.yarkin@gmail.com>
2019-10-10 20:48:15 -0400

    update patches

--- app-image/debug-build.sh
+++ app-image/debug-build.sh
@@ -5,11 +5,13 @@
 #   -v ${PWD}/tmp:/srv/asiou/asiou \
 #   yarsttec/asiou:7.6-base /bin/bash
 
+# rm -rf /srv/asiou/asiou/*
+
 mkdir -p ${WWW_HOME}/{asiou,scripts,patches}
 cp -r /mnt/home/* ${WWW_HOME}/
 chmod +x $WWW_HOME/scripts/*.sh $WWW_HOME/patches/*.sh
 
-export ASIOU_VERSION=7.6
+export ASIOU_VERSION=7.6.1
 export CACHE_DISTR_FILE=yes
 $WWW_HOME/scripts/install-asiou.sh
 
